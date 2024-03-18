ea740cb674aed1f093682c18e85b0babcd807aa9
hxufcdsuy <161042520+hxufcdsuy@users.noreply.github.com>
2024-03-18 11:28:19 -0300

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<locale.h>
+int main(){
+	setlocale(LC_ALL,"portuguese");
+	//variavel
+	int numero;
+	
+printf("numero:");
+scanf("%i",&numero);
+printf ("o antecessor é: %i",numero -1);
+printf("\no sucessor é:%i",numero +1)	;
+	
+return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
