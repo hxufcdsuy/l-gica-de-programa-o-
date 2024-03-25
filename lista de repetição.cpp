469b312e6488f6aa06ed907f16aa9ca0343275d1
hxufcdsuy <161042520+hxufcdsuy@users.noreply.github.com>
2024-03-25 08:54:23 -0300

    Add files via upload

---
+++ lista de repetição.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+int main(){ 
+int j;
+for(j=10;j >=1;j--){printf("%d\n",j);
+}
+return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
