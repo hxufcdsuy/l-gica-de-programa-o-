162729f7535a9d32396380e583c014ca57e1de75
hxufcdsuy <161042520+hxufcdsuy@users.noreply.github.com>
2024-04-05 11:02:49 -0300

    Add files via upload

---
+++ c while e do.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<locale.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<limits.h>
+int main(){
+setlocale(LC_ALL,"");
+//variavel
+int opcao,numerodefilhos,contadorfilho=0,totalfamilia=0,somasalario=0;
+float salario,salariomaior=INT_MAX,salariomin=INT_MIN,mediasalarial;
+
+//dados do e while
+do{ 
+        printf("=== MENU ===");
+   
+
+
+   
+    
+	printf("\ndigite quantos integrantes tem na sua familia:");
+	scanf("%i",&totalfamilia);
+    
+	 printf("NUMERO DE FILHOS:");
+	scanf("%i",&numerodefilhos);o final 
+    
+	
+	
+	
+}	while(salario);
+	
+	
+	
+	
+}
