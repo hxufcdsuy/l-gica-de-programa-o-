45bf9d4a8a2e6a64033e8cd8bae5305fb75a72c8
hxufcdsuy <161042520+hxufcdsuy@users.noreply.github.com>
2024-04-18 10:01:22 -0300

    Add files via upload

---
+++ do e while revisão.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<string.h>
+int main(){
+	
+int contador=0;
+char prosseguir;
+do{
+
+printf("digite 'c' se deseja continuar");
+scanf("%i",&prosseguir);
+
+
+	
+}
+while(prosseguir == 'c');  {
+	
+	 printf("o bloco foi repetido %d vezes \n ",contador);
+
+	
+}	
+	
+	
+	
+	
+	
+	
+}
