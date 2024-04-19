0fd121f332cc42c1b6487e3736ea9c8f6f008753
hxufcdsuy <161042520+hxufcdsuy@users.noreply.github.com>
2024-04-19 11:30:16 -0300

    Add files via upload

---
+++ matriz de media ponderada.c
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+#include<string.h>
+#define TAM 3
+int main(){
+	//i=nota
+	//j=media
+	
+float notas[3][3],media=0;
+int	pesoum[3],pesodois[4],pesotres[3];
+char materia[TAM];
+int i,j;
+
+
+for(i=0;i<TAM,i++){
+	printf("\n digite a sua %iª matéria",i+1);
+	scanf("%i",&materia[TAM]);
+	
+}for(i=0;i<3;i++) {
+	
+do{
+	
+	
+	
+	
+	
+}	
+	
+	
+}
+
+	
+	
+	
+
+
+ 	
+ 	
+ 	
+ 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
